544535af557cdc9d7aa532e36a87c0861394140c
Pkl CI <pkl-oss@group.apple.com>
2024-02-02 20:16:27 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/com.influxdata.telegraf/1.0.0/plugins/inputs/DiskInput/index.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/com.influxdata.telegraf/1.0.0/plugins/inputs/DiskInput/index.js
@@ -1,1 +1,1 @@
-runtimeData.links('known-versions','[{"text":"1.0.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"1.0.1","href":"../../../../1.0.1/plugins/inputs/DiskInput/index.html"},{"text":"1.0.0","classes":"current-version"}]');

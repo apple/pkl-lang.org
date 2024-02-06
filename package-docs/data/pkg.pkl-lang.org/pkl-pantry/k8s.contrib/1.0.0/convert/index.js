9ed01d65776f273231918427fa4f3f5f756c9023
Pkl CI <pkl-oss@group.apple.com>
2024-02-06 13:46:24 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/k8s.contrib/1.0.0/convert/index.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/k8s.contrib/1.0.0/convert/index.js
@@ -1,1 +1,1 @@
-runtimeData.links('known-versions','[{"text":"1.0.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"1.0.1","href":"../../1.0.1/convert/index.html"},{"text":"1.0.0","classes":"current-version"}]');

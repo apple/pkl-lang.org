0d2727e1d0632fd918569af3eb3ace0e4c9aba86
Pkl CI <pkl-oss@group.apple.com>
2024-02-21 11:43:06 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/org.json_schema.contrib/1.0.0/internal/Type/index.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/org.json_schema.contrib/1.0.0/internal/Type/index.js
@@ -1,2 +1,2 @@
-runtimeData.links('known-versions','[{"text":"1.0.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"1.0.1","href":"../../../1.0.1/internal/Type/index.html"},{"text":"1.0.0","classes":"current-version"}]');
 runtimeData.links('known-usages','[{"text":"utils","href":"../utils/index.html"}]');

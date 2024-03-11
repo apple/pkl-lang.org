180d58ff4ba503e6b3bcfdc63edd2b1c0675e2b4
Pkl CI <pkl-oss@group.apple.com>
2024-03-11 18:21:33 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-go/pkl.golang/0.5.0/go/index.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-go/pkl.golang/0.5.0/go/index.js
@@ -1,1 +1,1 @@
-runtimeData.links('known-versions','[{"text":"0.5.3","href":"../../0.5.3/go/index.html"},{"text":"0.5.2","href":"../../0.5.2/go/index.html"},{"text":"0.5.1","href":"../../0.5.1/go/index.html"},{"text":"0.5.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"0.6.0","href":"../../0.6.0/go/index.html"},{"text":"0.5.3","href":"../../0.5.3/go/index.html"},{"text":"0.5.2","href":"../../0.5.2/go/index.html"},{"text":"0.5.1","href":"../../0.5.1/go/index.html"},{"text":"0.5.0","classes":"current-version"}]');
---
+++ package-docs/data/pkg.pkl-lang.org/pkl-go/pkl.golang/0.6.0/go/Name.js
@@ -0,0 +1,1 @@
+runtimeData.links('known-versions','[{"text":"0.6.0","classes":"current-version"},{"text":"0.5.3","href":"../../0.5.3/go/Name.html"},{"text":"0.5.2","href":"../../0.5.2/go/Name.html"},{"text":"0.5.1","href":"../../0.5.1/go/Name.html"},{"text":"0.5.0","href":"../../0.5.0/go/Name.html"}]');

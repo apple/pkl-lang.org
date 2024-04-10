2639b67ec2416174da76683c29ccdaca5b714b32
Pkl CI <pkl-oss@group.apple.com>
2024-04-10 15:39:27 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/org.json_schema.contrib/1.0.0/generate/index.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/org.json_schema.contrib/1.0.0/generate/index.js
@@ -1,1 +1,1 @@
-runtimeData.links('known-versions','[{"text":"1.0.3","href":"../../1.0.3/generate/index.html"},{"text":"1.0.2","href":"../../1.0.2/generate/index.html"},{"text":"1.0.1","href":"../../1.0.1/generate/index.html"},{"text":"1.0.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"1.0.4","href":"../../1.0.4/generate/index.html"},{"text":"1.0.3","href":"../../1.0.3/generate/index.html"},{"text":"1.0.2","href":"../../1.0.2/generate/index.html"},{"text":"1.0.1","href":"../../1.0.1/generate/index.html"},{"text":"1.0.0","classes":"current-version"}]');
---
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/org.json_schema.contrib/1.0.4/generate/index.js
@@ -0,0 +1,1 @@
+runtimeData.links('known-versions','[{"text":"1.0.4","classes":"current-version"},{"text":"1.0.3","href":"../../1.0.3/generate/index.html"},{"text":"1.0.2","href":"../../1.0.2/generate/index.html"},{"text":"1.0.1","href":"../../1.0.1/generate/index.html"},{"text":"1.0.0","href":"../../1.0.0/generate/index.html"}]');

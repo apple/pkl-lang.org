0d725752098058fb99072e44f8d3bdc197cdaa31
Pkl CI <pkl-oss@group.apple.com>
2024-06-27 17:46:21 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/com.influxdata.telegraf/1.1.0/Telegraf/Processors.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/com.influxdata.telegraf/1.1.0/Telegraf/Processors.js
@@ -1,2 +1,2 @@
-runtimeData.links('known-versions','[{"text":"1.1.1","href":"../../1.1.1/Telegraf/Processors.html"},{"text":"1.1.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"1.2.0","href":"../../1.2.0/Telegraf/Processors.html"},{"text":"1.1.1","href":"../../1.1.1/Telegraf/Processors.html"},{"text":"1.1.0","classes":"current-version"}]');
 runtimeData.links('known-usages','[{"text":"Telegraf","href":"index.html"}]');
---
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/com.influxdata.telegraf/1.2.0/plugins/outputs/OpenTelemetryOutput/index.js
@@ -0,0 +1,2 @@
+runtimeData.links('known-versions','[{"text":"1.2.0","classes":"current-version"}]');
+runtimeData.links('known-usages','[{"text":"Outputs","href":"../../../Telegraf/Outputs.html"}]');

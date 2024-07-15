529e3d8e1076b548aa691e17a90faa7de1d436a8
Pkl CI <pkl-oss@group.apple.com>
2024-07-15 10:08:54 +0000

    Publish docsite [skip ci]

---
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/pkl.experimental.deepToTyped/1.0.3/deepToTyped/index.js
@@ -0,0 +1,1 @@
+runtimeData.links('known-versions','[{"text":"1.0.3","classes":"current-version"},{"text":"1.0.2","href":"../../1.0.2/deepToTyped/index.html"},{"text":"1.0.1","href":"../../1.0.1/deepToTyped/index.html"},{"text":"1.0.0","href":"../../1.0.0/deepToTyped/index.html"}]');
---
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/pkl.experimental.structuredRead/1.0.0/structuredRead/Failure.js
@@ -0,0 +1,2 @@
+runtimeData.links('known-versions','[{"text":"1.0.0","classes":"current-version"}]');
+runtimeData.links('known-subtypes','[{"text":"CoerceFailure","href":"CoerceFailure.html"},{"text":"ReadFailure","href":"ReadFailure.html"}]');

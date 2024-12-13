d6e43c2468db297693d043559cfcdb8360de00b7
Pkl CI <pkl-oss@group.apple.com>
2024-12-13 20:17:15 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/pkl.experimental.structuredRead/1.0.0/structuredRead/Failure.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/pkl.experimental.structuredRead/1.0.0/structuredRead/Failure.js
@@ -1,2 +1,2 @@
-runtimeData.links('known-versions','[{"text":"1.0.2","href":"../../1.0.2/structuredRead/Failure.html"},{"text":"1.0.1","href":"../../1.0.1/structuredRead/Failure.html"},{"text":"1.0.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"1.0.3","href":"../../1.0.3/structuredRead/Failure.html"},{"text":"1.0.2","href":"../../1.0.2/structuredRead/Failure.html"},{"text":"1.0.1","href":"../../1.0.1/structuredRead/Failure.html"},{"text":"1.0.0","classes":"current-version"}]');
 runtimeData.links('known-subtypes','[{"text":"CoerceFailure","href":"CoerceFailure.html"},{"text":"ReadFailure","href":"ReadFailure.html"}]');
--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/pkl.experimental.structuredRead/1.0.1/structuredRead/Failure.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/pkl.experimental.structuredRead/1.0.1/structuredRead/Failure.js
@@ -1,2 +1,2 @@
-runtimeData.links('known-versions','[{"text":"1.0.2","href":"../../1.0.2/structuredRead/Failure.html"},{"text":"1.0.1","classes":"current-version"},{"text":"1.0.0","href":"../../1.0.0/structuredRead/Failure.html"}]');
+runtimeData.links('known-versions','[{"text":"1.0.3","href":"../../1.0.3/structuredRead/Failure.html"},{"text":"1.0.2","href":"../../1.0.2/structuredRead/Failure.html"},{"text":"1.0.1","classes":"current-version"},{"text":"1.0.0","href":"../../1.0.0/structuredRead/Failure.html"}]');
 runtimeData.links('known-subtypes','[{"text":"CoerceFailure","href":"CoerceFailure.html"},{"text":"ReadFailure","href":"ReadFailure.html"}]');

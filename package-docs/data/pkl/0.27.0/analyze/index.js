d1dc8d60283d3246109d21e8bd83ec0ab9392c16
Pkl CI <pkl-oss@group.apple.com>
2025-01-22 23:42:20 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkl/0.27.0/analyze/index.js
+++ package-docs/data/pkl/0.27.0/analyze/index.js
@@ -1,1 +1,1 @@
-runtimeData.links('known-versions','[{"text":"0.27.1","href":"../../0.27.1/analyze/index.html"},{"text":"0.27.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"0.27.2","href":"../../0.27.2/analyze/index.html"},{"text":"0.27.1","href":"../../0.27.1/analyze/index.html"},{"text":"0.27.0","classes":"current-version"}]');

9885416cbaa5a4ff31c859657abc840a094963d6
Pkl CI <pkl-oss@group.apple.com>
2025-01-24 16:52:31 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-swift/pkl.swift/0.3.0/swift/Name.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-swift/pkl.swift/0.3.0/swift/Name.js
@@ -1,1 +1,1 @@
-runtimeData.links('known-versions','[{"text":"0.3.0","classes":"current-version"},{"text":"0.2.3","href":"../../0.2.3/swift/Name.html"}]');
+runtimeData.links('known-versions','[{"text":"0.4.0","href":"../../0.4.0/swift/Name.html"},{"text":"0.3.0","classes":"current-version"},{"text":"0.2.3","href":"../../0.2.3/swift/Name.html"}]');
---
+++ package-docs/data/pkg.pkl-lang.org/pkl-swift/pkl.swift/0.4.0/GeneratorSettings/index.js
@@ -0,0 +1,1 @@
+runtimeData.links('known-versions','[{"text":"0.4.0","classes":"current-version"},{"text":"0.3.0","href":"../../0.3.0/GeneratorSettings/index.html"},{"text":"0.2.3","href":"../../0.2.3/GeneratorSettings/index.html"}]');

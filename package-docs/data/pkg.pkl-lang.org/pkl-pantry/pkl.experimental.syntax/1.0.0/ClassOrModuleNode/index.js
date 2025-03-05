23099d910611ddbca2454a953987629687303c88
Pkl CI <pkl-oss@group.apple.com>
2025-03-05 21:46:42 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/pkl.experimental.syntax/1.0.0/ClassOrModuleNode/index.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/pkl.experimental.syntax/1.0.0/ClassOrModuleNode/index.js
@@ -1,2 +1,2 @@
-runtimeData.links('known-versions','[{"text":"1.0.3","href":"../../1.0.3/ClassOrModuleNode/index.html"},{"text":"1.0.2","href":"../../1.0.2/ClassOrModuleNode/index.html"},{"text":"1.0.1","href":"../../1.0.1/ClassOrModuleNode/index.html"},{"text":"1.0.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"1.1.0","href":"../../1.1.0/ClassOrModuleNode/index.html"},{"text":"1.0.3","href":"../../1.0.3/ClassOrModuleNode/index.html"},{"text":"1.0.2","href":"../../1.0.2/ClassOrModuleNode/index.html"},{"text":"1.0.1","href":"../../1.0.1/ClassOrModuleNode/index.html"},{"text":"1.0.0","classes":"current-version"}]');
 runtimeData.links('known-subtypes','[{"text":"ClassNode","href":"../ClassNode/index.html"}]');

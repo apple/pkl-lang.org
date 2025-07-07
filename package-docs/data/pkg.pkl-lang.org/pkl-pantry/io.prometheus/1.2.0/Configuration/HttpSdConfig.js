d4621018f79a1c8fc9971c872f191ca7162d6bb6
Pkl CI <pkl-oss@group.apple.com>
2025-07-07 18:18:17 +0000

    Publish docsite [skip ci]

--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/io.prometheus/1.2.0/Configuration/HttpSdConfig.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/io.prometheus/1.2.0/Configuration/HttpSdConfig.js
@@ -1,2 +1,2 @@
-runtimeData.links('known-versions','[{"text":"1.3.0","href":"../../1.3.0/Configuration/HttpSdConfig.html"},{"text":"1.2.0","classes":"current-version"}]');
+runtimeData.links('known-versions','[{"text":"1.4.0","href":"../../1.4.0/Configuration/HttpSdConfig.html"},{"text":"1.3.0","href":"../../1.3.0/Configuration/HttpSdConfig.html"},{"text":"1.2.0","classes":"current-version"}]');
 runtimeData.links('known-usages','[{"text":"AlertManagerConfig","href":"AlertManagerConfig.html"},{"text":"ScrapeConfig","href":"ScrapeConfig.html"}]');
--- package-docs/data/pkg.pkl-lang.org/pkl-pantry/io.prometheus/1.3.0/Configuration/HttpSdConfig.js
+++ package-docs/data/pkg.pkl-lang.org/pkl-pantry/io.prometheus/1.3.0/Configuration/HttpSdConfig.js
@@ -1,2 +1,2 @@
-runtimeData.links('known-versions','[{"text":"1.3.0","classes":"current-version"},{"text":"1.2.0","href":"../../1.2.0/Configuration/HttpSdConfig.html"}]');
+runtimeData.links('known-versions','[{"text":"1.4.0","href":"../../1.4.0/Configuration/HttpSdConfig.html"},{"text":"1.3.0","classes":"current-version"},{"text":"1.2.0","href":"../../1.2.0/Configuration/HttpSdConfig.html"}]');
 runtimeData.links('known-usages','[{"text":"AlertManagerConfig","href":"AlertManagerConfig.html"},{"text":"ScrapeConfig","href":"ScrapeConfig.html"}]');
